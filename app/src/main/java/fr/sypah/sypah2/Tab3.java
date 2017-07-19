7be7aebcde5262aa785fc7bd94e50127497c1fd8
Druidoroxx <tay-son.ho@edu.esiee.fr>
2017-07-19 03:07:05 +0200

    littlestep

--- app/src/main/java/fr/sypah/sypah2/Tab3.java
+++ app/src/main/java/fr/sypah/sypah2/Tab3.java
@@ -5,11 +5,15 @@ package fr.sypah.sypah2;
 /**
  * Created by Thibaud on 16/06/2017.
  */
+import android.app.Activity;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class Tab3 extends Fragment {
 
@@ -17,11 +21,38 @@ public class Tab3 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.activity_tab3, container, false);
+        Spinner spinner = (Spinner) rootView.findViewById(R.id.spinner1);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(),
+                R.array.Theme_array, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
         return rootView;
 
 
 
 
 
+
+
+
+
     }
+
+    public class SpinnerActivity extends Activity implements AdapterView.OnItemSelectedListener {
+
+
+        public void onItemSelected(AdapterView<?> parent, View view,
+                                   int pos, long id) {
+            // An item was selected. You can retrieve the selected item using
+            // parent.getItemAtPosition(pos)
+        }
+
+        public void onNothingSelected(AdapterView<?> parent) {
+            // Another interface callback
+        }
+    }
+
 }
